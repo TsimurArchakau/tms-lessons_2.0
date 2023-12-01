3726c25e5c9f2d59391729859bc565d8c0f5dd6b
TsimurArchakau <timniktimnik@gmail.com>
2023-12-01 19:57:31 +0100

    test commit

---
+++ lesson_selenium/test_main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+
+class TestSelenium:
+    def test_selenium_First(self):
+        driver = webdriver.Chrome()
+        driver.get("https://www.python.org/")
+        current_url = driver.current_url
+
+        assert "python" in current_url
